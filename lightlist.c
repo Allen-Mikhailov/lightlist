71e6df052238be6844305ba7c8391f6294f73624
Allen-Mikhailov <mikhailovallen@gmail.com>
2025-08-17 21:01:12 -0500

    Working on command line and parsing

--- lightlist.c
+++ lightlist.c
@@ -78,6 +78,66 @@ void create_new_list_button(int id)
 
 }
 
+char **parse_command(char* str)
+{
+	int length = strlen(str);
+
+	int allocated_arguments = 2;
+	int arguments = 0;
+
+	int buffer_allocated = length+2;
+	int buffer_used = 0;
+
+	char **commands = malloc(sizeof(char*) * 2);
+	char *command_buffer = malloc(sizeof(char) * buffer_allocated);
+
+	// Parse input
+	int in_string = 0;
+	int in_escape_character;
+	int was_space_last = 0;
+	for (int i = 0; i < length; i++)
+	{
+		char c = command_line_input[i];
+		if (in_escape_character)
+		{
+			was_space_last = 0;
+			continue;
+		}
+
+		if (c == ' ' && !was_space_last) {
+			was_space_last = 1;
+			arguments++;
+
+			if (arguments > allocated_arguments)
+				commands = realloc(commands, sizeof(char*) * arguments);
+
+			continue;
+		}
+
+		if (c == '\\')
+			in_escape_character = 1;
+
+		else if (c == '"')
+			in_string = !in_string;
+		else {
+
+		}
+
+		was_space_last = 0;
+
+	}
+
+	return commands;
+
+
+}
+
+void free_command_args(char **args)
+{
+	free(args[0]);
+	free(args);
+}
+
 // TODO: Make this call a function so we can free or something and shove in a function
 TextMenu directory_menu;
 
@@ -130,6 +190,11 @@ void pick_list()
 		printw("Frame: %d\n", frame);
 		printw("%d\n", last_input);
 
+		if (in_command)
+		{
+			printw("Command Line:\n%s", command_line_input);
+		}
+
 
 		// Writes to the screen
 		refresh();
@@ -138,23 +203,44 @@ void pick_list()
 		last_input = getch();
 
 		// Handling input
-		if (last_input == KEY_UP)
+		if (last_input == KEY_UP || last_input == 'k')
 			text_menu_move(&directory_menu, -1);
 
 		if (last_input == KEY_DOWN)
-			text_menu_move(&directory_menu, 1);
+			text_menu_move(&directory_menu, 1 || last_input == 'j');
 
 		if (last_input == 27) // ESC Key
 			 exit = 1;
 
 		if (last_input == 10) // Enter Key
 		{
+						
 
+			// Clearing the string
+			command_line_input[0] = '\0';
+			in_command = 0;
+		}
+		
+		// open command line
+		if (last_input == ':' && !in_command)
+		{
+			in_command = 1;
+			command_line_input[0] = '\0';
+			command_length = 0;
+			// The colon will get added by the next if statement
 		}
 
-		if (last_input >= 33 && last_input <= 126)
+		if (last_input >= 33 && last_input <= 126 && in_command)
 		{
-			
+			command_line_input[command_length] = (char) last_input;
+			command_line_input[command_length+1] = '\0';
+			command_length++;
+		}
+
+		if (last_input == 263 && command_length > 0) // Backspace
+		{
+			command_line_input[command_length-1] = '\0';
+			command_length--;
 		}
 
 
